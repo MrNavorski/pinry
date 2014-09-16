c6b36e1e3905e16d513e7714cc6737e5be522a7f
Mrnavorski <lu.shiyong@zuimeia.com>
2014-09-16 17:07:28 +0800

    add wsgi file

--- uwsgi_wsgi_product.py
+++ uwsgi_wsgi_product.py
@@ -5,7 +5,7 @@ reload(sys)
 
 if not os.path.dirname(__file__) in sys.path[:1]:
     sys.path.insert(0, os.path.dirname(__file__))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'pinry.production'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pinry.settings.production'
 
 from django.core.handlers.wsgi import WSGIHandler
 application = WSGIHandler()
